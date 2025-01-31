2ec7773d27f730e5f2453caa61a68d1ef0e95fe3
Dani81mz <danielnessfotografia@gmail.com>
2025-01-31 20:17:58 +0100

    wa fuera

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const hamburger = document.querySelector("#hamburger")
-const enlaces = document.querySelector("#nav_links")
+const hamburger = document.querySelector("#hamburger");
+const enlaces = document.querySelector(".nav_links")
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("show")
-})
+});
